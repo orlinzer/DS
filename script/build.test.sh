8d133430400fd47e3652cab1bf4a1d3345c36cbd
Or Linzer <orlinzer@gmail.com>
2021-12-12 08:56:37 +0200

    fix stop on crash

--- script/build.test.sh
+++ script/build.test.sh
@@ -11,8 +11,9 @@ echo "Build Tests:"
 if [ -d "../src/test/" ]; then
   mkdir -p "../bin/test/"
 fi
-g++ -std="c++11" -o "../bin/test/AVLTree.test" -I "../include" "../src/test/AVLTree.test.cpp"
-g++ -std="c++11" -o "../bin/test/KeyValue.test" -I "../include" "../src/test/KeyValue.test.cpp"
+
+g++ -std="c++11" -o "../bin/test/AVLTree.test" -I "../include" "../src/test/AVLTree.test.cpp" || exit
+g++ -std="c++11" -o "../bin/test/KeyValue.test" -I "../include" "../src/test/KeyValue.test.cpp" || exit
 
 # Return to the old path
 cd "$OLD_PATH" || exit
--- script/build_and_run.test.sh
+++ script/build_and_run.test.sh
@@ -6,8 +6,8 @@ OLD_PATH=$(pwd)
 # Change to this directory
 cd "$(dirname "$0")" || exit
 
-./build.test.sh
-./run.test.sh
+./build.test.sh || exit
+./run.test.sh || exit
 
 # Return to the old path
 cd "$OLD_PATH" || exit
